806add39109ca0658b3f4e79fd6c25da5019ecdc
wahyusa <wahyuskull22@gmail.com>
2024-03-14 15:02:38 +0700

    feat: :card_file_box: Main.GO will auto migrate schema.go
    The schema will changes later if I realize something

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"go-learn/schema"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// Average global var
+var dbcon *gorm.DB
+
 func main() {
-	//TODO
+	// Setup DB Connection
+	dbcon = ConnectionStart()
+
+	// generate tables
+	dbcon.AutoMigrate(&schema.User{}, &schema.Photo{}, &schema.Comment{}, &schema.SocialMedia{})
+
+}
+
+func ConnectionStart() *gorm.DB {
+	dsn := "host=localhost user=postgres password=password dbname=my_gram port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
